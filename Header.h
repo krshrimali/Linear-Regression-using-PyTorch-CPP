22acae37827cf162d250d50bf1cfe1c09d910c4d
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2019-12-17 13:35:09 +0000

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+class CSVRow
+{
+public:
+	float operator[](std::size_t index)
+	{
+		std::string& eg = m_data[index];
+		return std::stof(eg);
+	}
+	std::size_t size() const
+	{
+		return m_data.size();
+	}
+	void readNextRow(std::istream& str)
+	{
+		std::string         line;
+		std::getline(str, line);
+
+		std::stringstream   lineStream(line);
+		std::string         cell;
+
+		m_data.clear();
+		while (std::getline(lineStream, cell, ','))
+		{
+			m_data.push_back(cell);
+		}
+		// This checks for a trailing comma with no data after it.
+		if (!lineStream && cell.empty())
+		{
+			// If there was a trailing comma then add an empty element.
+			m_data.push_back("");
+		}
+	}
+private:
+	std::vector<std::string>    m_data;
+};
+
+std::istream& operator>>(std::istream& str, CSVRow& data)
+{
+	data.readNextRow(str);
+	return str;
+}
